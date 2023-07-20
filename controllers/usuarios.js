16ada682763842a5703931907bf7b865fc08701a
EduardoEnriquezG <104819314+EduardoEnriquezG@users.noreply.github.com>
2023-07-19 19:43:39 -0600

    Primer commit backend

---
+++ controllers/usuarios.js
@@ -0,0 +1,71 @@
+const { response } = require('express');
+
+
+const usuariosGet = (req, res = response) => {
+
+    const {q, nombre = 'No name', apikey, page = 1, limit} = req.query;
+
+    res.json(
+        {
+          ok: true,
+          msg: 'get API - Controlador',
+          q,
+          nombre,
+          apikey,
+          page,
+          limit
+        }
+    );
+}
+
+const usuariosPost = (req, res = response) => {
+
+    const {nombre , edad} = req.body;
+
+    res.json(
+        {
+          ok: true,
+          msg: 'post API - Controlador',
+          body
+        }
+    );
+}
+
+const usuariosPut = (req, res = response) => {
+
+    const id = req.params.id;
+
+    res.json(
+        {
+          ok: true,
+          msg: 'put API - Controlador',
+          "id": id 
+        }
+    );
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json(
+        {
+          ok: true,
+          msg: 'patch API - Controlador'
+        }
+    );
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json(
+        {
+          ok: true,
+          msg: 'delete API - Controlador'
+        }
+    );
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete
+} 
